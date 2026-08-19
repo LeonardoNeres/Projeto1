6a764969ce3409706ceda0cc0f1133fc802e968b
LeonardoNeres <leonardo.neres2010@gmail.com>
2026-08-19 12:23:14 -0300

    Liberando CORS no backend

--- meu-backend/server.js
+++ meu-backend/server.js
@@ -43,10 +43,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Middlewares Globais
-app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
-  credentials: true
-}));
+app.use(cors({ origin: 'https://devburguer-tau.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
